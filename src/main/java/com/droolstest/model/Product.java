19d3c099f06e21b129a8bd9e655e38b7000e1036
mark andrew <mark.andrew@gmail.com>
2017-09-05 18:11:34 +0200

    minimal correct build for drools hello world runnable as jar

---
+++ src/main/java/com/droolstest/model/Product.java
@@ -0,0 +1,24 @@
+package com.droolstest.model;
+
+public class Product {
+	
+	private String type;
+	private int discount;
+
+	public String getType() {
+		return type;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+}
